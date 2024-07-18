611de4092741db1389aab815facdf4618854e458
joincheol <jju161613@naver.com>
2024-07-19 00:01:26 +0900

    Add click star function

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ for (let i=0; i<starNumber; i++){
       scale(${pointTo[2]})`}
   ];
   options = {
-    duration: 3000 / scale,
+    duration: 5000 / scale,
     iterations: Infinity,
     fill: 'both',
     easing: 'linear'
--- star.js
+++ star.js
@@ -3,6 +3,15 @@ export default class Star{
     this.element = document.createElement('div');
     this.element.classList.add('star');
     this.element.innerHTML = number;
+
+    this.element.addEventListener('click', ()=>{
+      const lists = document.querySelector('.choose_list');
+      const list = document.createElement('li');
+      list.classList.add('check_word');
+      list.innerHTML = 'word';
+      lists.append(list);
+    });
+    
     document.body.append(this.element);
   }
 }

c1c7fc75412b29f42b1a64eaeeebea2f4106d967
joincheol <jju161613@naver.com>
2024-07-18 17:01:15 +0900

    Add tag and style

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ for (let i=0; i<starNumber; i++){
       scale(${pointTo[2]})`}
   ];
   options = {
-    duration: 1000 / scale,
+    duration: 3000 / scale,
     iterations: Infinity,
     fill: 'both',
     easing: 'linear'
